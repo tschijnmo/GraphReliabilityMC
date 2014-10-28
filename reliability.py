5b3a5f2d4b132ad46e850232efd3d118e898ce04
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-10-27 23:00:53 -0500

    Add new reliability measure
    A new measure for the reliability is added, where the ratio between
    simple paths before and after the failure is used. This is able to offer
    more rigorous measure at the cost of very high computational complexity.

--- reliability.py
+++ reliability.py
@@ -52,6 +52,16 @@ def if_conn(graph):
     return nx.has_path(graph, first_node, last_node)
 
 
+def count_simple_path(graph):
+
+    """Counts the number of simple path from the first to the last node"""
+
+    nodes = graph.nodes()
+    first_node = nodes[0]
+    last_node = nodes[-1]
+    return len(list(nx.all_simple_paths(graph, first_node, last_node)))
+
+
 def gen_graph_w_failure(graph, rate):
 
     """Generates a graph from a given graph with nodes fail by a given rate
@@ -74,7 +84,7 @@ def gen_graph_w_failure(graph, rate):
     return graph.subgraph(new_nodes)
 
 
-def do_one_MC(graph, rate):
+def do_one_MC(graph, rate, count_path=False):
 
     """Performs one MC simulation
 
@@ -84,10 +94,13 @@ def do_one_MC(graph, rate):
     """
 
     sub_graph = gen_graph_w_failure(graph, rate)
-    return if_conn(sub_graph)
+    if count_path:
+        return count_simple_path(sub_graph)
+    else:
+        return if_conn(sub_graph)
 
 
-def compute_reliability(graph, rate, n_samples):
+def compute_reliability(graph, rate, n_samples, count_path=False):
 
     """Compute the reliability by MC method
 
@@ -96,8 +109,18 @@ def compute_reliability(graph, rate, n_samples):
 
     """
 
-    result = [do_one_MC(graph, rate) for i in xrange(0, n_samples)]
-    return len([i for i in result if i]) * 1.0 / len(result)
+    if count_path:
+
+        init_path_cnt = count_simple_path(graph)
+        path_cnt_after = [
+            do_one_MC(graph, rate, True) for i in xrange(0, n_samples)
+            ]
+        ratios = [i * 1.0 / init_path_cnt for i in path_cnt_after]
+        return sum(ratios) / len(ratios)
+
+    else:
+        result = [do_one_MC(graph, rate, False) for i in xrange(0, n_samples)]
+        return len([i for i in result if i]) * 1.0 / len(result)
 
 
 #
@@ -121,10 +144,15 @@ def main():
                         help='The failure rate')
     parser.add_argument('-s', '--sample', default=2000, type=int,
                         help='The number of sampling in the MC simulation')
+    parser.add_argument('-c', '--count-paths', default=False,
+                        action='store_true', help='If count the actual number'
+                        'of simple paths')
     args = parser.parse_args()
 
     graph = read_graph(args.mat, args.name)
-    reliability = compute_reliability(graph, args.rate, args.sample)
+
+    reliability = compute_reliability(graph, args.rate,
+                                      args.sample, args.count_paths)
 
     print(
         'After %d MC simulation, the network with %d nodes' % (args.sample,
